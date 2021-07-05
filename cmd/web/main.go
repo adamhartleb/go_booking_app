04a63e9f784d9a83be8cee27bcf4bd9310c72ca5
Adam <adamjhartleb@gmail.com>
2021-07-05 16:55:58 -0700

    lec 39

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,30 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/adamhartleb/go_booking_app/pkg/config"
 	"github.com/adamhartleb/go_booking_app/pkg/handlers"
 	"github.com/adamhartleb/go_booking_app/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const PORT = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Unable to create Template Cache")
 	}
 
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
 	app.TemplateCache = tc
 	app.UseCache = false
-
-	mux := Routes(&app)
+	app.InProduction = false
+	app.Session = session
 
 	repo := handlers.NewRepo(&app)
 	render.NewTemplates(&app)
 	handlers.NewHandlers(repo)
+
+	mux := Routes(&app)
 
 	http.ListenAndServe(PORT, mux)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/adamhartleb/go_booking_app
 go 1.16
 
 require (
+	github.com/alexedwards/scs/v2 v2.4.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.3 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
